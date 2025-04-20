768096d27652f9acf4f55f2a042476145ea4e7d5
MahoMuri <paolo.santos_12@yahoo.com.ph>
2025-04-20 22:45:55 +0800

    feat: add typing text animation

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,30 +1,21 @@
 "use client";
 
-import TypingText from "@/components/typing-text";
-import { Button } from "@/components/ui/button";
-import { useState } from "react";
+import { TypeAnimation } from "react-type-animation";
 
 export default function Home() {
-	const [hobby, setHobby] = useState("sleep");
-
 	return (
 		<div>
 			<div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
 				<h1>Hello I am Paolo</h1>
 				<h2 className="flex gap-2">
 					I love to:{" "}
-					<TypingText
-						text={` ${hobby}`}
-						repeat={false}
-						onComplete={() =>
-							setTimeout(() => {
-								setHobby("code");
-							}, 3000)
-						}
-						grow
+					<TypeAnimation
+						className="text-blue-400"
+						sequence={["code", 3000, "learn", 3000, "play", 3000]}
+						wrapper="span"
+						repeat={Infinity}
 					/>
 				</h2>
-				<Button onClick={() => setHobby("learn")}>Click ME</Button>
 			</div>
 
 			<div>
