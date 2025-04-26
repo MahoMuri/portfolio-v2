4c0c1bd65ba1c8bc8ced4f75161c322d24183203
MahoMuri <paolo.santos_12@yahoo.com.ph>
2025-04-26 12:18:01 +0800

    feat: add rotating text animation

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -9,13 +9,12 @@ export default function HomeLayout({
 		<div className="relative h-svh">
 			<Particles
 				className="border border-b-white/10"
-				particleColors={["#afefb3", "#56f060"]}
-				particleCount={800}
+				particleColors={["#2b7fff", "#00b8db", "#00c951"]}
+				particleCount={500}
 				particleSpread={10}
-				speed={0.5}
+				speed={1}
 				particleBaseSize={100}
 				moveParticlesOnHover={true}
-				alphaParticles={false}
 				disableRotation={true}
 			/>
 			{children}
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,19 +1,42 @@
 "use client";
 
-import { TypeAnimation } from "react-type-animation";
+import RotatingText from "@/components/text-animations/RotatingText/RotatingText";
+import SplitText from "@/components/text-animations/SplitText/SplitText";
 
 export default function Home() {
 	return (
 		<div>
-			<div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
-				<h1>Hello I am Paolo</h1>
-				<h2 className="flex gap-2">
-					I love to:{" "}
-					<TypeAnimation
-						className="text-blue-400"
-						sequence={["code", 3000, "learn", 3000, "play", 3000]}
-						wrapper="span"
-						repeat={Infinity}
+			<div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col gap-4">
+				<h1>
+					<SplitText
+						text="Hello, I am Paolo!"
+						threshold={0.2}
+						rootMargin="-50px"
+					/>
+				</h1>
+				<h2 className="flex gap-2 items-center">
+					<p className="pb-0.5 sm:pb-1 md:pb-1">I love to:</p>
+					<RotatingText
+						texts={[
+							"build",
+							"design",
+							"develop",
+							"code",
+							"learn",
+							"play",
+						]}
+						mainClassName="px-2 sm:px-2 md:px-3 bg-linear-to-r from-blue-500 via-cyan-500 to-green-500 overflow-hidden text-black py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+						initial={{ y: "-100%" }}
+						animate={{ y: 0 }}
+						exit={{ y: "150%" }}
+						staggerDuration={0.05}
+						splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+						transition={{
+							type: "spring",
+							damping: 30,
+							stiffness: 400,
+						}}
+						rotationInterval={2000}
 					/>
 				</h2>
 			</div>
