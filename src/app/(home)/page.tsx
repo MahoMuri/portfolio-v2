4695f6df5c4d0f2311f715609f743ff4348f5477
MahoMuri <paolo.santos_12@yahoo.com.ph>
2025-05-01 16:55:21 +0800

    feat: add entrance animation

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,49 +1,81 @@
 "use client";
 
 import RotatingText from "@/components/text-animations/RotatingText/RotatingText";
-import SplitText from "@/components/text-animations/SplitText/SplitText";
+import { Separator } from "@/components/ui/separator";
+import { motion } from "framer-motion";
+import Image from "next/image";
 
 export default function Home() {
 	return (
-		<div>
-			<div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col gap-4">
-				<h1>
-					<SplitText
-						text="Hello, I am Paolo!"
-						threshold={0.2}
-						rootMargin="-50px"
-					/>
-				</h1>
-				<h2 className="flex gap-2 items-center">
-					<p className="pb-0.5 sm:pb-1 md:pb-1">I love to:</p>
-					<RotatingText
-						texts={[
-							"build",
-							"design",
-							"develop",
-							"code",
-							"learn",
-							"play",
-						]}
-						mainClassName="px-2 sm:px-2 md:px-3 bg-linear-to-r from-blue-500 via-cyan-500 to-green-500 overflow-hidden text-black py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
-						initial={{ y: "-100%" }}
+		<main>
+			<div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center">
+				<div className="overflow-clip pr-8">
+					<motion.div
+						className="rounded-3xl bg-contain size-80 overflow-clip"
+						initial={{ x: 400, opacity: 0 }}
+						animate={{ x: 0, opacity: 1 }}
+						transition={{ duration: 1, delay: 1.2 }}
+					>
+						<Image
+							className="-translate-y-3"
+							src="/profile.jpg"
+							alt="profile"
+							width={400}
+							height={600}
+						/>
+					</motion.div>
+				</div>
+				<div className="overflow-clip">
+					<motion.div
+						initial={{ y: -400 }}
 						animate={{ y: 0 }}
-						exit={{ y: "150%" }}
-						staggerDuration={0.05}
-						splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
-						transition={{
-							type: "spring",
-							damping: 30,
-							stiffness: 400,
-						}}
-						rotationInterval={2000}
-					/>
-				</h2>
+						transition={{ duration: 1 }}
+					>
+						<Separator
+							orientation="vertical"
+							className="py-40 bg-foreground/50"
+						/>
+					</motion.div>
+				</div>
+				<div className="overflow-clip pl-8">
+					<motion.div
+						className="flex flex-col gap-4"
+						initial={{ x: -450, opacity: 0 }}
+						animate={{ x: 0, opacity: 1 }}
+						transition={{ duration: 1, delay: 1.2 }}
+					>
+						<h1>Hello, I am Paolo.</h1>
+						<h1 className="flex gap-2 items-center">
+							<p className="pb-0.5 sm:pb-1 md:pb-1">I love to</p>
+							<RotatingText
+								texts={[
+									"build",
+									"design",
+									"develop",
+									"code",
+									"learn",
+									"play",
+								]}
+								mainClassName="px-4 pt-2 pb-4 bg-linear-to-r from-blue-500 via-cyan-500 to-green-500 overflow-clip rounded-lg"
+								initial={{ y: "-150%" }}
+								animate={{ y: 0 }}
+								exit={{ y: "150%" }}
+								staggerDuration={0.05}
+								transition={{
+									type: "spring",
+									damping: 30,
+									stiffness: 400,
+								}}
+								rotationInterval={2000}
+							/>
+						</h1>
+					</motion.div>
+				</div>
 			</div>
 
-			<div>
-				<h2>Some content</h2>
+			<div className="">
+				<h2>Projects</h2>
 			</div>
-		</div>
+		</main>
 	);
 }
