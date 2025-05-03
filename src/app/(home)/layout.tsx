7b25973e061d6f73ae02d6e7f84efc8dca8819e1
MahoMuri <paolo.santos_12@yahoo.com.ph>
2025-05-03 18:26:15 +0800

    add description

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -6,15 +6,14 @@ export default function HomeLayout({
 	children,
 }: Readonly<{ children: React.ReactNode }>) {
 	return (
-		<div className="relative h-svh">
+		<div className="relative h-svh border border-white/0 border-b-white/20">
 			<Particles
-				className="border border-b-white/10"
 				particleColors={["#2b7fff", "#00b8db", "#00c951"]}
 				particleCount={500}
 				particleSpread={10}
 				speed={1}
 				particleBaseSize={100}
-				moveParticlesOnHover={true}
+				moveParticlesOnHover={false}
 				disableRotation={true}
 			/>
 			{children}
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -11,18 +11,24 @@ export default function Home() {
 			<div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center">
 				<div className="overflow-clip pr-8">
 					<motion.div
-						className="rounded-3xl bg-contain size-80 overflow-clip"
 						initial={{ x: 400, opacity: 0 }}
 						animate={{ x: 0, opacity: 1 }}
 						transition={{ duration: 1, delay: 1.2 }}
 					>
-						<Image
-							className="-translate-y-3"
-							src="/profile.jpg"
-							alt="profile"
-							width={400}
-							height={600}
-						/>
+						<motion.div
+							className="overflow-clip size-80"
+							initial={{ borderRadius: 0 }}
+							animate={{ borderRadius: "1.5rem" }}
+							transition={{ duration: 1, delay: 2.2 }}
+						>
+							<Image
+								className="-translate-y-3"
+								src="/profile.jpg"
+								alt="profile"
+								width={400}
+								height={600}
+							/>
+						</motion.div>
 					</motion.div>
 				</div>
 				<div className="overflow-clip">
@@ -39,36 +45,47 @@ export default function Home() {
 				</div>
 				<div className="overflow-clip pl-8">
 					<motion.div
-						className="flex flex-col gap-4"
+						className="flex flex-col"
 						initial={{ x: -450, opacity: 0 }}
 						animate={{ x: 0, opacity: 1 }}
 						transition={{ duration: 1, delay: 1.2 }}
 					>
-						<h1>Hello, I am Paolo.</h1>
-						<h1 className="flex gap-2 items-center">
-							<p className="pb-0.5 sm:pb-1 md:pb-1">I love to</p>
-							<RotatingText
-								texts={[
-									"build",
-									"design",
-									"develop",
-									"code",
-									"learn",
-									"play",
-								]}
-								mainClassName="px-4 pt-2 pb-4 bg-linear-to-r from-blue-500 via-cyan-500 to-green-500 overflow-clip rounded-lg"
-								initial={{ y: "-150%" }}
-								animate={{ y: 0 }}
-								exit={{ y: "150%" }}
-								staggerDuration={0.05}
-								transition={{
-									type: "spring",
-									damping: 30,
-									stiffness: 400,
-								}}
-								rotationInterval={2000}
-							/>
-						</h1>
+						<p className="text-3xl mb-2 font-thin">Hello,</p>
+						<div className="text-primary">
+							<h1 className="text-7xl w-max font-sans">
+								I&apos;m Paolo Santos
+							</h1>
+							<div className="text-2xl font-mono my-0 font-medium overflow-clip">
+								<RotatingText
+									texts={[
+										"💻 Web Developer",
+										"🎸 Musician",
+										"🔴 Streamer",
+										"🎮 Gamer",
+									]}
+									initial={{ y: "-150%" }}
+									animate={{ y: 0 }}
+									exit={{ y: "150%" }}
+									staggerDuration={0.015}
+									transition={{
+										type: "spring",
+										damping: 30,
+										stiffness: 600,
+									}}
+									rotationInterval={3000}
+								/>
+							</div>
+						</div>
+						<p className="text-lg font-thin">
+							a front-end developer based in the
+							Philippines with a passion for creating visually
+							stunning and highly functional web applications.
+							I&apos;m also a talented musician, an avid gamer and
+							a charismatic streamer. With over 2 years of
+							experience in web development, I&apos;m confident in
+							my ability to deliver high-quality results and
+							continuously learn new things.
+						</p>
 					</motion.div>
 				</div>
 			</div>
